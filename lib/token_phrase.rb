e605303fcb2ec115934d21aeb9fa48cc8b3b447e
Eric Steele <eric@notvelcro.com>
2013-01-15 15:02:18 -0500

    gave dictionary methods arguments

--- lib/token_phrase.rb
+++ lib/token_phrase.rb
@@ -6,20 +6,20 @@ module TokenPhrase
   Patterns = %w(striped checked spotted polka-dotted plaid wavy houndstooth argyle glossy matte pinstriped)
   Nouns = %w(floutist carpenter jacket president address machine computer mug lamp phone wall bicycle river lake fountain building book hat pants shirt cape soup gloves pen suit photograph sand profit energy fork compact-disk floppy-disk chandelier door window laboratory people tapir wolverine wolf spider wolf-spider spider-wolf banana-slug giraffe deer-mouse capybara dingo dragon cardinal owl octopus elk moose weasel elephant rhino iguana bullfrog greyhound stickbug ladybug ant rat coyote chimpanzee housecat barracuda raven crane fox panda racoon nessie whale dolphin shark viper frog toad flounder skunk wookie dishwasher bat space-heater bobble-head lego-set pinboard flag tv video-game envelope headphones mousepad jukebox)
   
-  def self.adjectives
-    TokenPhrase::Adjectives
+  def self.adjectives(more_adjectives = nil)
+    more_adjectives.nil? ? TokenPhrase::Adjectives : TokenPhrase::Adjectives | more_adjectives
   end
   
-  def self.colors
-    TokenPhrase::Colors
+  def self.colors(more_colors = nil)
+    more_colors.nil? ? TokenPhrase::Colors : TokenPhrase::Colors | more_colors
   end
   
-  def self.patterns
-    TokenPhrase::Patterns
+  def self.patterns(more_patterns = nil)
+    more_patterns.nil? ? TokenPhrase::Patterns : TokenPhrase::Patterns | more_patterns
   end
   
-  def self.nouns
-    TokenPhrase::Nouns
+  def self.nouns(more_nouns = nil)
+    more_nouns.nil? ? TokenPhrase::Nouns : TokenPhrase::Nouns | more_nouns
   end
   
   def self.generate(separator = nil, dictionaries = {})
