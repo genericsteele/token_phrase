24358c3c5ec2fb54c0e71b60b1babf03c5d4e645
Eric Steele <eric@notvelcro.com>
2013-07-20 20:09:45 -0400

    added hash-rocket syntax in tests

--- test/generator_test.rb
+++ test/generator_test.rb
@@ -4,17 +4,17 @@ require 'token_phrase'
 
 class GeneratorTest < MiniTest::Unit::TestCase
   def test_that_numbers_can_be_excluded
-    phrase = TokenPhrase.generate numbers: false
+    phrase = TokenPhrase.generate :numbers => false
     refute_match /\d+$/, phrase
   end
 
   def test_that_permutations_calculate_all_combinations
     permutations = TokenPhrase.permutations(
-      adjectives: %w(test),
-      colors: %w(test),
-      patterns: %w(test),
-      nouns: %w(test),
-      numbers: false
+      :adjectives => %w(test),
+      :colors => %w(test),
+      :patterns => %w(test),
+      :nouns => %w(test),
+      :numbers => false
     )
     assert_equal 1, permutations
   end
@@ -26,22 +26,22 @@ class GeneratorTest < MiniTest::Unit::TestCase
   end
 
   def test_that_adjectives_can_be_overridden
-    phrase = TokenPhrase.generate adjectives: %w(test)
+    phrase = TokenPhrase.generate :adjectives => %w(test)
     assert_match /test/, phrase
   end
 
   def test_that_colors_can_be_overridden
-    phrase = TokenPhrase.generate colors: %w(test)
+    phrase = TokenPhrase.generate :colors => %w(test)
     assert_match /test/, phrase
   end
   
   def test_that_patterns_can_be_overridden
-    phrase = TokenPhrase.generate patterns: %w(test)
+    phrase = TokenPhrase.generate :patterns => %w(test)
     assert_match /test/, phrase
   end
   
   def test_that_nouns_can_be_overridden
-    phrase = TokenPhrase.generate nouns: %w(test)
+    phrase = TokenPhrase.generate :nouns => %w(test)
     assert_match /test/, phrase
   end
 
